d2838a7a50e2434d885c877404740c6f87dd94fc
Maksim Gorbachewski <gorbachewski.m@gmail.com>
2021-11-12 15:48:04 +0300

    first

---
+++ Snake/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Snake
+{
+    class Game
+    {
+        public class Snake
+        {
+
+        }
+
+        public Snake sn = new Snake();
+        public char[,] field = new char[Console.WindowHeight, Console.WindowWidth];
+
+        public Game()
+        {
+            for (int i = 0; i < Console.WindowHeight; i++)
+                for (int j = 0; j < Console.WindowHeight; j++)
+                    this.field[i, j] = ' ';
+
+        }
+    
+    }
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            while(true)
+            {
+                var key = Console.ReadKey();
+            }
+            
+        }
+    }
+}
