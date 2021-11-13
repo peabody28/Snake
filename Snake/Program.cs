42d297aa1db9c37d1883e475d0b52d7f8b480d66
Maksim Gorbachewski <gorbachewski.m@gmail.com>
2021-11-13 11:43:38 +0300

    v2 + bot

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -102,7 +102,8 @@ namespace Snake
 
         public Point GetNextPoint()
         {
-            Point next = snake[snake.Count - 1];
+            int ind = snake.Count - 1;
+            Point next = snake[ind];
           
             switch (direction)
             {
@@ -131,18 +132,18 @@ namespace Snake
                 {
                     case Direction.LEFT:
                     case Direction.RIGHT:
-                        if (key == ConsoleKey.S)
+                        if (key == ConsoleKey.S || key == ConsoleKey.DownArrow)
                             direction = Direction.DOWN;
-                        else if (key == ConsoleKey.W)
+                        else if (key == ConsoleKey.W || key == ConsoleKey.UpArrow)
                             direction = Direction.UP;
                         break;
 
 
                     case Direction.UP:
                     case Direction.DOWN:
-                        if (key == ConsoleKey.A)
+                        if (key == ConsoleKey.A || key == ConsoleKey.LeftArrow)
                             direction = Direction.LEFT;
-                        else if (key == ConsoleKey.D)
+                        else if (key == ConsoleKey.D || key == ConsoleKey.RightArrow)
                             direction = Direction.RIGHT;
                         break;
                 }
@@ -175,7 +176,7 @@ namespace Snake
         }
     }
 
-    class FoodFactory
+    public class FoodFactory
     {
         int x;
         int y;
@@ -225,8 +226,39 @@ namespace Snake
             DrawHorizontal(x, y);
             DrawVertical(0, y);
             DrawVertical(x, y);
+
+            //GenWalls();
         }
 
+        public void GenWalls()
+        {
+            Random rn = new Random();
+
+            #region Horizonatal
+            int y = rn.Next() % (Console.WindowHeight-1);
+            int len = rn.Next(10, 30);
+            int begin = rn.Next(10, Console.WindowWidth-1) % (Console.WindowWidth-1);
+            for (int i = begin; i < begin+len; i++)
+            {
+                Point p = new Point(i, y, ch);
+                p.Draw();
+                wall.Add(p);
+            }
+            #endregion
+
+            #region Vertical
+            int x = rn.Next(2, Console.WindowHeight-1) % (Console.WindowHeight-1);
+            len = rn.Next(10, 30);
+            begin = rn.Next(10, Console.WindowHeight-1) % Console.WindowHeight-1;
+            for (int i = begin; i < begin+len; i++)
+            {
+                Point p = new Point(x, i, ch);
+                p.Draw();
+                wall.Add(p);
+            }
+            #endregion
+
+        }
         private void DrawHorizontal(int x, int y)
         {
             for (int i = 0; i < x; i++)
@@ -260,6 +292,79 @@ namespace Snake
         }
     }// class Walls
 
+    public class Bot
+    {
+        public void Move(Snake snake, FoodFactory foodFactory)
+        {
+            // основное действие - осуществление поворота
+            // 1. проверка на необходимость поворота (либо движение к еде, либо уход от столкновения)
+            // 2. выбор направления поворота
+
+            int dx = snake.head.x - foodFactory.food.x;
+            int dy = snake.head.y - foodFactory.food.y;
+
+            if (dx == 0)
+            {
+                // x-ы равны
+                // свожу y                       
+                if (dy < 0 && snake.direction != Direction.DOWN)
+                {
+
+                    if (snake.direction != Direction.UP && snake.direction != Direction.DOWN)
+                        snake.Rotation(ConsoleKey.DownArrow);
+                    else
+                    {
+                        if (dx < 0)
+                            snake.Rotation(ConsoleKey.RightArrow);
+                        else
+                            snake.Rotation(ConsoleKey.LeftArrow);
+                    }
+                }
+                else if (dy > 0 && snake.direction != Direction.UP)
+                {
+                    if (snake.direction != Direction.UP && snake.direction != Direction.DOWN)
+                        snake.Rotation(ConsoleKey.UpArrow);
+                    else
+                    {
+                        if (dx < 0)
+                            snake.Rotation(ConsoleKey.RightArrow);
+                        else
+                            snake.Rotation(ConsoleKey.LeftArrow);
+                    }
+                }
+            }
+            else
+            {
+                if (dx > 0 && snake.direction != Direction.LEFT)
+                {
+                    if (snake.direction != Direction.RIGHT && snake.direction != Direction.LEFT)
+                        snake.Rotation(ConsoleKey.LeftArrow);
+                    else
+                    {
+                        if (dy < 0)
+                            snake.Rotation(ConsoleKey.DownArrow);
+                        else
+                            snake.Rotation(ConsoleKey.UpArrow);
+                    }
+
+                }
+                else if (dx < 0 && snake.direction != Direction.RIGHT)
+                {
+                    if (snake.direction != Direction.RIGHT && snake.direction != Direction.LEFT)
+                        snake.Rotation(ConsoleKey.RightArrow);
+                    else
+                    {
+                        if (dy > 0)
+                            snake.Rotation(ConsoleKey.DownArrow);
+                        else
+                            snake.Rotation(ConsoleKey.UpArrow);
+                    }
+                }
+            }
+
+        }
+
+    }
     public class Game
     {
         static readonly int x = Console.WindowWidth;
@@ -269,27 +374,50 @@ namespace Snake
         static FoodFactory foodFactory;
         static Walls walls;
         static Timer time;
-        static void Main(string[] args)
+        static Bot bot;
+        static bool game_over = false;
+
+        static int speed = 100;
+        static void Main()
         {
+
             Console.SetWindowSize(x + 1, y + 1);
             Console.SetBufferSize(x + 1, y + 1);
             Console.CursorVisible = false;
 
+            
             snake = new Snake(x / 2, y / 2, 3);
             walls = new Walls(x, y, '#');
             foodFactory = new FoodFactory(x, y, '@');
             foodFactory.CreateFood(snake);
+            bot = new Bot();
 
-            time = new Timer(Loop, null, 0, 100);
+            time = new Timer(Loop, null, 0, speed);
 
             while (true)
             {
+                //bot.Move(snake, foodFactory);
+                if (game_over)
+                    break;
                 if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey(true);
                     snake.Rotation(key.Key);
                 }
+
             }
+            Console.SetCursorPosition(30, 30);
+            Console.Write("████─████─█───█─███   ████─█─█─███─████");
+            Console.SetCursorPosition(30, 31);
+            Console.Write("█────█──█─██─██─█     █──█─█─█─█───█──█");
+            Console.SetCursorPosition(30, 32);
+            Console.Write("█─██─████─█─█─█─███   █──█─█─█─███─████");
+            Console.SetCursorPosition(30, 33);
+            Console.Write("█──█─█──█─█───█─█     █──█─███─█───█─█");
+            Console.SetCursorPosition(30, 34);
+            Console.Write("████─█──█─█───█─███   ████──█──███─█─█");
+            Console.ReadLine();
+            
 
         }
 
@@ -298,10 +426,13 @@ namespace Snake
             if (walls.IsHit(snake.snake[snake.snake.Count-1]) || snake.IsHit(snake.snake[snake.snake.Count - 1]))
             {
                 time.Change(0, Timeout.Infinite);
+                game_over = true;
             }
             else if (snake.Eat(foodFactory.food))
             {
                 foodFactory.CreateFood(snake);
+                //speed = (int)speed - (int)(speed * 0.08);
+                time.Change(0, speed);
             }
             else
             {
